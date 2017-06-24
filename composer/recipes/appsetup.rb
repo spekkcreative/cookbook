53dc71770ed237319cecf807844978e5de4a6829
Brad Filip <brad@bradfilip.com>
2017-06-24 17:22:46 -0400

    new test 5.6

--- composer/recipes/appsetup.rb
+++ composer/recipes/appsetup.rb
@@ -5,7 +5,23 @@ node[:deploy].each do |app_name, deploy|
     cwd "#{deploy[:deploy_to]}/current"
     code "curl -sS https://getcomposer.org/installer | php && php composer.phar install --no-dev"
   end
-  yum_package 'php56' do
-      action :install
+end
+
+apt_repository 'php5-5.6' do
+  uri node['php5_ppa']['uri']
+  key node['php5_ppa']['key']
+  keyserver node['php5_ppa']['key_server']
+  components ['trusty', 'main']
+end
+
+# Install Git using new source repo
+package 'php5-fpm' do
+  action :install
+end
+
+#Install PHP modules
+node['php5_ppa']['modules'].each do |install_packages|
+  package install_packages do
+    action :install
   end
 end
