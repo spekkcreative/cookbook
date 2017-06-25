f458f4e9c9b94bda7fa2e95b9aca20714648eb7a
Brad Filip <brad@bradfilip.com>
2017-06-25 09:48:52 -0400

    roll back

--- composer/recipes/appsetup.rb
+++ composer/recipes/appsetup.rb
@@ -6,22 +6,3 @@ node[:deploy].each do |app_name, deploy|
     code "curl -sS https://getcomposer.org/installer | php && php composer.phar install --no-dev"
   end
 end
-
-apt_repository 'php5-5.6' do
-  uri node['php5_ppa']['uri']
-  key node['php5_ppa']['key']
-  keyserver node['php5_ppa']['key_server']
-  components ['trusty', 'main']
-end
-
-# Install Git using new source repo
-package 'php5-fpm' do
-  action :install
-end
-
-#Install PHP modules
-node['php5_ppa']['modules'].each do |install_packages|
-  package install_packages do
-    action :install
-  end
-end
